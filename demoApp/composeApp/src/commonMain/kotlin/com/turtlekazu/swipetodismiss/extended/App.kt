dd4b1c1acaf3d14c4bd0b484b6e11dd777c09304
Kazuhiro ISHIBASHI <turtlekazu@icloud.com>
2025-12-13 15:27:10 +0900

    feat(demoApp): move composeApp/iosApp to demoApp directory and fix paths

--- demoApp/composeApp/src/commonMain/kotlin/com/turtlekazu/swipetodismiss/extended/App.kt
+++ demoApp/composeApp/src/commonMain/kotlin/com/turtlekazu/swipetodismiss/extended/App.kt
@@ -10,14 +10,17 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import swipe_to_dismiss_extended.composeapp.generated.resources.Res
-import swipe_to_dismiss_extended.composeapp.generated.resources.compose_multiplatform
+import swipe_to_dismiss_extended.demoapp.composeapp.generated.resources.Res
+import swipe_to_dismiss_extended.demoapp.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
