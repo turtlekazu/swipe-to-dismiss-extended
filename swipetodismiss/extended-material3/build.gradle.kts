cbe3c1cdf448ee64f0fd0ea452ed3aa90d001556
Kazuhiro ISHIBASHI <turtlekazu@icloud.com>
2025-12-13 18:26:53 +0900

    feat(gradle): add Compose dependencies and refactor hard-coded sdk versions

--- swipetodismiss/extended-material3/build.gradle.kts
+++ swipetodismiss/extended-material3/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidKotlinMultiplatformLibrary)
     alias(libs.plugins.androidLint)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
 }
 
 kotlin {
@@ -11,8 +13,8 @@ kotlin {
     // See: https://kotlinlang.org/docs/multiplatform-discover-project.html#targets
     androidLibrary {
         namespace = "com.turtlekazu.swipetodismiss.extended.material3"
-        compileSdk = 36
-        minSdk = 24
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
 
         withHostTestBuilder {
         }
@@ -61,6 +63,9 @@ kotlin {
             dependencies {
                 implementation(libs.kotlin.stdlib)
                 // Add KMP dependencies here
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.material3)
             }
         }
 
